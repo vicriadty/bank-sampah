a9d3ce3f0709b1fd2ec3f279efdc311cc9b4dc69
vicriadty <adityavicri@gmail.com>
2026-07-14 08:19:05 +0700

    fix: responsive form filter transaksi saat browser zoom (fix typo col md-3, flex-wrap, g-2)

--- resources/views/admin/transaksi/setor-sampah/index.blade.php
+++ resources/views/admin/transaksi/setor-sampah/index.blade.php
@@ -54,32 +54,24 @@
 
     </div>
     {{-- Form Filter --}}
-    <form method="GET" action="{{ route('admin.setoran.index') }}" class="row mb-3">
-        <div class="col-md-2">
-            <input type="text" name="nasabah" class="form-control" placeholder="Cari Nama Nasabah"
-                value="{{ request('nasabah') }}">
-        </div>
-        <div class="col-md-2 d-flex ">
-            <input type="date" name="tanggal_awal" class="form-control" value="{{ request('tanggal_awal') }}">
-            <div class="ml-3 d-flex align-items-center">
-                <span>s/d</span>
+    <form method="GET" action="{{ route('admin.setoran.index') }}" class="mb-3">
+        <div class="row g-2 align-items-end">
+            <div class="col-12 col-md-2">
+                <input type="text" name="nasabah" class="form-control" placeholder="Cari Nama Nasabah"
+                    value="{{ request('nasabah') }}">
             </div>
-        </div>
-        <div class="col-md-2">
-            <input type="date" name="tanggal_akhir" class="form-control" value="{{ request('tanggal_akhir') }}">
-        </div>
-        <div class="col md-3">
-            <button type="submit" name="action" value="filter" class="btn btn-primary mr-2">Filter</button>
-            <a href="{{ route('admin.setoran.index') }}" class="btn btn-secondary mr-5">Reset</a>
-        </div>
-        <div class="col-md-3 d-flex justify-content-end">
-
-            <button type="submit" name="action" value="cetak" class="btn btn-danger mr-2"><i class="fas fa-file-pdf"></i>
-                Cetak
-                Laporan</button>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalSetoran"><i
-                    class="fas fa-plus fa-sm text-white-50"></i> Tambah
-                Setoran</button>
+            <div class="col-6 col-md-2">
+                <input type="date" name="tanggal_awal" class="form-control" value="{{ request('tanggal_awal') }}">
+            </div>
+            <div class="col-6 col-md-2">
+                <input type="date" name="tanggal_akhir" class="form-control" value="{{ request('tanggal_akhir') }}">
+            </div>
+            <div class="col-12 col-md-4 d-flex flex-wrap gap-2">
+                <button type="submit" name="action" value="filter" class="btn btn-primary">Filter</button>
+                <a href="{{ route('admin.setoran.index') }}" class="btn btn-secondary">Reset</a>
+                <button type="submit" name="action" value="cetak" class="btn btn-danger"><i class="fas fa-file-pdf"></i> Cetak Laporan</button>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalSetoran"><i class="fas fa-plus fa-sm text-white-50"></i> Tambah Setoran</button>
+            </div>
         </div>
     </form>
 
